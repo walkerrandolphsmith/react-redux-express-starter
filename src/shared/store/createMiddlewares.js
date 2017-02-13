4d9172d07f0613e9a526b6fb4a0d2df0201ba381
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-02-12 21:14:02 -0500

    Syntatic suggar to the middleware builder

--- src/shared/store/createMiddlewares.js
+++ src/shared/store/createMiddlewares.js
@@ -3,9 +3,8 @@ import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import promiseMiddleware from './../middleware/promiseMiddleware';
 
-export default (history) => {
-    return applyMiddleware(
-        thunk,
-        promiseMiddleware,
-        routerMiddleware(history));
-}
+export default (history) => applyMiddleware(
+    thunk,
+    promiseMiddleware,
+    routerMiddleware(history)
+);
