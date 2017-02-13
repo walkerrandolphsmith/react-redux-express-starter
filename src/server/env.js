7d50fdf0bbd7d9c309c416e1b2655e1f02032983
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-02-12 21:12:17 -0500

    Deafult env values

--- src/server/env.js
+++ src/server/env.js
@@ -1,4 +1,10 @@
 const DEFAULT_HOST = 'localhost';
+const DEFAULT_MONGO_URI = 'mongodb://localhost/starter-app';
+const DEFAULT_PORT = 3004;
+const DEFAULT_DEV_PORT = 3001;
+const DEFAULT_API_PORT = 3002;
+const DEFAULT_NODE_ENV = 'production';
+
 const {
     VERSION_NUMBER,
     NODE_ENV,
@@ -10,14 +16,14 @@ const {
 } = process.env;
 
 export const isBrowser = process.browser;
-export const nodeEnv = NODE_ENV || 'production';
+export const nodeEnv = NODE_ENV || DEFAULT_NODE_ENV;
 export const versionNumber = VERSION_NUMBER;
 export const host = HOST || DEFAULT_HOST;
-export const port = PORT || 3000;
+export const port = PORT || DEFAULT_PORT;
 export const devHost = DEV_HOST || DEFAULT_HOST;
-export const devPort = DEV_PORT || 3001;
+export const devPort = DEV_PORT || DEFAULT_DEV_PORT;
 export const apiHost = API_HOST || DEFAULT_HOST;
-export const apiPort = API_PORT || 3002;
-export const mongoUri = MONDO_URI || 'mongodb://localhost/starter';
+export const apiPort = API_PORT || DEFAULT_API_PORT;
+export const mongoUri = MONDO_URI || DEFAULT_MONGO_URI;
 export const mailgunApiKey = MAILGUN_API_KEY;
 export const mailgunDomain = MAILGUN_DOMAIN;
